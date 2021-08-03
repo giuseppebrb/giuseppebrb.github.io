ecdf1b553bd5bdd566592e958dd214ba17421e9d
Giuseppe Barbato <giuseppe95barbato@gmail.com>
2021-08-03 21:12:11 +0200

    Changed eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,13 +7,12 @@ module.exports = {
   },
   rules: {
     'import/no-extraneous-dependencies': 'off',
-    'prettier/prettier': 'error',
     'no-console': 'warn',
     'react/jsx-one-expression-per-line': 'off',
     'react/jsx-filename-extension': 'off', // disable if necessary
     'react/static-property-placement': 'off', // disable if necessary
     'react/jsx-props-no-spreading': 'off', // disable if necessary
-    'react/require-default-props': 'off',
+    'react/require-default-props': 'off'
   },
   parser: 'babel-eslint',
 };

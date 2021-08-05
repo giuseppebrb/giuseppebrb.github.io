b98a064da04e9255ce0edb8ba6eeb01ccd28f3f8
Giuseppe Barbato <giuseppe95barbato@gmail.com>
2021-08-05 13:00:34 +0200

    Skills section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,11 +7,13 @@ import Footer from './Footer/Footer';
 
 import { PortfolioProvider } from '../context/context';
 
-import { heroData, aboutData, projectsData, contactData, footerData } from '../mock/data';
+import { heroData, aboutData, skillsData, projectsData, contactData, footerData } from '../mock/data';
+import Skills from './Skills/Skills';
 
 function App() {
   const [hero, setHero] = useState({});
   const [about, setAbout] = useState({});
+  const [skills, setSkills] = useState([]);
   const [projects, setProjects] = useState([]);
   const [contact, setContact] = useState({});
   const [footer, setFooter] = useState({});
@@ -19,15 +21,17 @@ function App() {
   useEffect(() => {
     setHero({ ...heroData });
     setAbout({ ...aboutData });
+    setSkills([...skillsData]);
     setProjects([...projectsData]);
     setContact({ ...contactData });
     setFooter({ ...footerData });
   }, []);
 
   return (
-    <PortfolioProvider value={{ hero, about, projects, contact, footer }}>
+    <PortfolioProvider value={{ hero, about, skills, projects, contact, footer }}>
       <Hero />
       <About />
+      <Skills />
       <Projects />
       <Contact />
       <Footer />
--- src/mock/data.js
+++ src/mock/data.js
@@ -20,9 +20,117 @@ export const aboutData = {
   img: 'profile.jpg',
   paragraphOne: '',
   paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphThree: ''
 };
+
+// SKILLS DATA
+export const skillsData = [
+  {
+    id: nanoid(),
+    img: 'angular-logo.png',
+    title: 'Angular',
+  },
+  {
+    id: nanoid(),
+    img: 'react-logo.png',
+    title: 'React',
+  },
+  {
+    id: nanoid(),
+    img: 'javascript-logo.png',
+    title: 'JavaScript',
+  },
+  {
+    id: nanoid(),
+    img: 'typescript-logo.png',
+    title: 'TypeScript',
+  },
+  {
+    id: nanoid(),
+    img: 'rxjs-logo.png',
+    title: 'RxJs',
+  },
+  {
+    id: nanoid(),
+    img: 'net-core-logo.png',
+    title: '.NET Core',
+  },
+  {
+    id: nanoid(),
+    img: 'aspnetcore-logo.png',
+    title: 'ASP.NET Core',
+  },
+  {
+    id: nanoid(),
+    img: 'csharp-logo.png',
+    title: 'C#',
+  },
+  {
+    id: nanoid(),
+    img: 'html5-logo.png',
+    title: 'HTML5',
+  },
+  {
+    id: nanoid(),
+    img: 'css-logo.png',
+    title: 'CSS3',
+  },
+  {
+    id: nanoid(),
+    img: 'sass-logo.png',
+    title: 'SASS',
+  },
+  {
+    id: nanoid(),
+    img: 'tailwind-logo.png',
+    title: 'TailwindCSS',
+  },
+  {
+    id: nanoid(),
+    img: 'bootstrap-logo.png',
+    title: 'Bootstrap',
+  },
+  {
+    id: nanoid(),
+    img: 'azure-logo.png',
+    title: 'Azure',
+  },
+  {
+    id: nanoid(),
+    img: 'git-logo.png',
+    title: 'Git',
+  },
+  {
+    id: nanoid(),
+    img: 'java-logo.png',
+    title: 'Java',
+  },
+  {
+    id: nanoid(),
+    img: 'python-logo.png',
+    title: 'Python',
+  },
+  {
+    id: nanoid(),
+    img: 'android-logo.png',
+    title: 'Android Dev',
+  },
+  {
+    id: nanoid(),
+    img: 'php-logo.png',
+    title: 'PHP',
+  },
+  {
+    id: nanoid(),
+    img: 'laravel-logo.png',
+    title: 'Laravel',
+  },
+  {
+    id: nanoid(),
+    img: 'wordpress-logo.png',
+    title: 'Wordpress',
+  }
+];
 
 // PROJECTS DATA
 export const projectsData = [
